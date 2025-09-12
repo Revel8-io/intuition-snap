9b921c32192da7e84689ee296796df503544f99d
Kylan Hurt <kylan.hurt@gmail.com>
2025-09-12 00:00:13 -0700

    Clean up config

--- packages/snap/src/config.ts
+++ packages/snap/src/config.ts
@@ -1,65 +1,56 @@
-import { base, baseSepolia } from 'viem/chains';
-
 export type ChainConfig = {
-  i7nContract: string;
   chainId: number;
   chainIdHex: string;
   chainName: string;
   chainKey: string;
-  explorerDomain: string;
-  i8nExplorerDomain: string;
+  i7nExplorerDomain: string;
   currencySymbol: string;
   decimalPrecision: number;
-  chain: typeof base | typeof baseSepolia;
-  ethRpcUrl: string;
   isAtomId: string;
   trustworthyAtomId: string;
-  relatedImagesAtomId: string;
   relatedNicknamesAtomId: string;
-  backendUrl: string;
-  chainlinkContractAddress: string;
 };
 
 export const BASE_MAINNET = {
   backendUrl: 'https://prod.base.intuition-api.com/v1/graphql',
-  i7nContract: '0x430BbF52503Bd4801E51182f4cB9f8F534225DE5',
   chainId: 8453,
   chainIdHex: '0x2105',
   chainName: 'Base Mainnet',
   chainKey: 'base-mainnet',
-  explorerDomain: 'https://basescan.org',
-  i8nExplorerDomain: 'https://beta.portal.intuition.systems/app',
+  i7nExplorerDomain: 'https://beta.portal.intuition.systems/app',
   currencySymbol: 'ETH',
   decimalPrecision: 18,
-  chain: base,
   trustworthyAtomId: '0', // todo: need to be created
   isAtomId: '0', // todo: need to be created
-  relatedImagesAtomId: '0',
   relatedNicknamesAtomId: '0',
-  ethRpcUrl:
-    'https://base-mainnet.g.alchemy.com/v2/xww_HDMOC0nGOVJL-HtXWxn2oqXOtK5v',
-  chainlinkContractAddress: '0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70',
 };
 
 export const BASE_SEPOLIA = {
   backendUrl: 'https://prod.base-sepolia.intuition-api.com/v1/graphql',
-  i7nContract: '0x1A6950807E33d5bC9975067e6D6b5Ea4cD661665',
   chainId: 84532,
   chainIdHex: '0x14A34',
   chainName: 'Base Sepolia',
   chainKey: 'base-sepolia',
-  explorerDomain: 'https://sepolia.basescan.org',
-  i8nExplorerDomain: 'https://dev.portal.intuition.systems/app',
+  i7nExplorerDomain: 'https://dev.portal.intuition.systems/app',
   currencySymbol: 'ETH',
   decimalPrecision: 18,
-  chain: baseSepolia,
   isAtomId: '26811',
   trustworthyAtomId: '26858',
-  relatedImagesAtomId: '26848',
   relatedNicknamesAtomId: '26813',
-  ethRpcUrl:
-    'https://base-sepolia.g.alchemy.com/v2/xww_HDMOC0nGOVJL-HtXWxn2oqXOtK5v',
-  chainlinkContractAddress: '0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1',
+};
+
+export const INTUITION_TESTNET = {
+  backendUrl: 'https://testnet.intuition.sh/v1/graphql',
+  chainId: 13579,
+  chainIdHex: '0x350B',
+  chainName: 'Intuition Testnet',
+  chainKey: 'intuition-testnet',
+  currencySymbol: 'TRUST',
+  decimalPrecision: 18,
+  i7nExplorerDomain: 'https://testnet.trust-scan.io',
+  isAtomId: '0',
+  trustworthyAtomId: '0',
+  relatedNicknamesAtomId: '0',
 };
 
 export const CHAIN_CONFIGS = [BASE_MAINNET, BASE_SEPOLIA];
@@ -84,10 +75,4 @@ export const getChainConfigByChainId = (
     throw new Error(`Chain config not found for chainId: ${chainId}`);
   }
   return chainConfig;
-};
-
-export const getChainConfigByChainIdHex = (chainIdHex: string) => {
-  return CHAIN_CONFIGS.find((config) => {
-    return config.chainIdHex.toLowerCase() === chainIdHex.toLowerCase();
-  });
 };
