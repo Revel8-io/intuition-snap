08e2c84dfa7e4e61f565e60e8b2ba73f72471dcb
Kylan Hurt <kylan.hurt@gmail.com>
2025-09-17 21:10:20 -0700

    Remove unused configs

--- packages/snap/src/config.ts
+++ packages/snap/src/config.ts
@@ -11,52 +11,23 @@ export type ChainConfig = {
   relatedNicknamesAtomId: string;
 };
 
-export const BASE_MAINNET = {
-  backendUrl: 'https://prod.base.intuition-api.com/v1/graphql',
-  chainId: 8453,
-  chainIdHex: '0x2105',
-  chainName: 'Base Mainnet',
-  chainKey: 'base-mainnet',
-  i7nExplorerDomain: 'https://beta.portal.intuition.systems/app',
-  currencySymbol: 'ETH',
-  decimalPrecision: 18,
-  trustworthyAtomId: '0', // todo: need to be created
-  isAtomId: '0', // todo: need to be created
-  relatedNicknamesAtomId: '0',
-};
-
-export const BASE_SEPOLIA = {
-  backendUrl: 'https://prod.base-sepolia.intuition-api.com/v1/graphql',
-  chainId: 84532,
-  chainIdHex: '0x14A34',
-  chainName: 'Base Sepolia',
-  chainKey: 'base-sepolia',
-  i7nExplorerDomain: 'https://dev.portal.intuition.systems/app',
-  currencySymbol: 'ETH',
-  decimalPrecision: 18,
-  isAtomId: '26811',
-  trustworthyAtomId: '26858',
-  relatedNicknamesAtomId: '26813',
-};
-
 export const INTUITION_TESTNET = {
   backendUrl: 'https://testnet.intuition.sh/v1/graphql',
   chainId: 13579,
   chainIdHex: '0x350B',
   chainName: 'Intuition Testnet',
   chainKey: 'intuition-testnet',
-  currencySymbol: 'TRUST',
+  currencySymbol: 'tTRUST',
   decimalPrecision: 18,
-  i7nExplorerDomain: 'https://testnet.trust-scan.io',
-  isAtomId: '0',
-  trustworthyAtomId: '0',
-  relatedNicknamesAtomId: '0',
+  i7nExplorerDomain: 'https://localhost:3000',
+  isAtomId: '0x9480992aaf84de3ead7ef7bc1eab16473de93647b83dc5c3575146d971b4737d',
+  trustworthyAtomId: '0x357a27d54fecf107f717eb144e660914beb907b83628dff7bb3ac0d51856afe8',
+  relatedNicknamesAtomId: '0xfed083dd8c0a10780db78d9643f7ee8bdccf8a67ffffee9fdf27dd7d3481dd0b',
 };
 
-export const CHAIN_CONFIGS = [BASE_MAINNET, BASE_SEPOLIA];
+export const CHAIN_CONFIGS = [INTUITION_TESTNET];
 
-export const chainConfig =
-  /* process.env.NODE_ENV === 'production' ? BASE_MAINNET : */ BASE_SEPOLIA;
+export const chainConfig = INTUITION_TESTNET
 
 export const getChainConfigByChainId = (
   chainId: number | string,
